b2856655c3571650619ec4d38533da5da351a79f
RaphaellyLargo <94926417+RaphaellyLargo@users.noreply.github.com>
2022-02-22 18:44:21 +0100

    push

---
+++ basicsjs.js
@@ -0,0 +1,19 @@
+//Operatoren
+
+//Arithmetic
+
+// Modulus
+console.log(5 % 3); // division, gibt 2 aus weil 2 rest.
+
+//Increment
+let count = 4;
+
+count;
+
+//Decrement
+
+//Unary Negation
+
+//Unary Plus
+
+//Exponential
